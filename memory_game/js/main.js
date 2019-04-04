10b85d4b86e47e6673a5ede291bff49f9d6b2c2e
Lyndsay Ramberg <lyndsayramberg@Lyndsays-MBP.fios-router.home>
2019-04-04 14:18:10 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,23 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[3];
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry try again");
+		console.log("Sorry try again");
 	}
-}
+};
+
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+flipCard(2);
+
